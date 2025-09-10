213e6dccde10d0baf88721f5ba956200fc9c6905
Caleb <cb8876@gmail.com>
2025-09-10 03:11:28 -0400

    started adding cunctionality for adding a DNA object

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
+        UserInteraction ui = new UserInteraction();
+        ui.mainMenu();
     }
 }
--- src/UserInteraction.java
+++ src/UserInteraction.java
@@ -5,14 +5,14 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class UserInteraction {
+    private final Scanner scanner = new Scanner(System.in);
 
 
-
-    public static String getDNA(String fileName) {
-        try (Scanner scanner = new Scanner(new FileInputStream(fileName))) {
+    public String getDNA(String fileName) {
+        try (Scanner fileRead = new Scanner(new FileInputStream(fileName))) {
             StringBuilder s = new StringBuilder();
-            while (scanner.hasNextLine()) {
-                s.append(scanner.nextLine());
+            while (fileRead.hasNextLine()) {
+                s.append(fileRead.nextLine());
             }
             return s.toString();
         }
@@ -22,20 +22,19 @@ public class UserInteraction {
         return null;
     }
 
-    public static String getFilePath() {
-        Scanner scanner = new Scanner(System.in);
+    public String getFilePath() {
         System.out.println("Enter path to DNA file: ");
-        return scanner.nextLine();
+        return this.scanner.nextLine();
     }
 
 
-    public static String getStartSequence() {
-        Scanner scanner = new Scanner(System.in);
+    public String getStartSequence() {
+
         String s = "";
 
         System.out.println("Enter the start codon to look for:");
         while (true) {
-            s = scanner.next().toUpperCase().trim();
+            s = this.scanner.next().toUpperCase().trim();
             if (!s.matches("[A-Z]{3}")) {
                 System.out.println("invalid codon");
             }
@@ -44,14 +43,13 @@ public class UserInteraction {
         return s;
     }
 
-    public static HashSet<String> getEndCodons() {
+    public HashSet<String> getEndCodons() {
         HashSet<String> codons = new HashSet<>();
-        Scanner scanner = new Scanner(System.in);
         String s = "";
 
         System.out.println("Enter the end codons to look for line by line (type end when done):");
         while (true) {
-            s = scanner.next().toUpperCase().trim();
+            s = this.scanner.next().toUpperCase().trim();
             if (!s.matches("[A-Z]{3}")) {
                 System.out.println("invalid codon");
             }
@@ -62,21 +60,53 @@ public class UserInteraction {
         return codons;
     }
 
-    public static void mainMenu() {
+
+
+    private String nameDNA() {
+
+    }
+
+    private void createDNA () {
+
+        String filePath = getFilePath();
+        String dna = getDNA(filePath);
+        String startSequence = getStartSequence();
+        HashSet<String> ends = getEndCodons();
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public void mainMenu() {
         Scanner scanner = new Scanner(System.in);
         int n = -1;
-        while (true) {
-            System.out.println("What would you like to do? :");
-            System.out.println("0: Save and Quit \n" +
-                    "1: Load a Save \n" +
-                    "2: Add a DNA Sequence \n" +
-                    "3: Load a DNA Sequence \n" +
-                    "4: Compare DNA Sequences");
+        boolean quit = false;
+        while (!quit) {
             try {
+                System.out.println("What would you like to do? :");
+                System.out.println("""
+                    0: Save and Quit\s
+                    1: Load a Save\s
+                    2: Add a DNA Sequence\s
+                    3: Load a DNA Sequence\s
+                    4: Edit a DNA Sequence\s
+                    5: Compare DNA Sequences""");
                 n = scanner.nextInt();
                 switch (n) {
                     case 0:
                         System.out.println("Add functionality for saving quiting");
+                        quit = true;
                         break;
                     case 1:
                         System.out.println("Add functionality for loading a save");
@@ -88,8 +118,13 @@ public class UserInteraction {
                         System.out.println("Add functionality for loading a DNA sequence");
                         break;
                     case 4:
+                        System.out.println("Add functionality for editing a DNA sequence");
+                        break;
+                    case 5:
                         System.out.println("Add functionality for comparing DNA sequences");
                         break;
+                    default:
+                        System.out.println("That is not a valid input");
                 }
             }
             catch (InputMismatchException e) {
